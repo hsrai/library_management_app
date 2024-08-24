42ba277bda693f24974d3308cce319fbd2fda19a
HS Rai <hardeep.rai@gmail.com>
2024-08-24 20:19:29 +0530

    Removed NameError: name 'Document' is not defined

--- library_management_app/library_management_app/doctype/article_name/article_name.py
+++ library_management_app/library_management_app/doctype/article_name/article_name.py
@@ -2,8 +2,10 @@
 # For license information, please see license.txt
 
 # import frappe
+#from frappe.model.document import Document
 from frappe.website.website_generator import WebsiteGenerator
 
 
-class ArticleName(Document):
+#class ArticleName(Document):
+class ArticleName(WebsiteGenerator):
 	pass
