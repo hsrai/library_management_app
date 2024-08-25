c231ac776f8b2d0e2a70e40f194a521fbe064104
HS Rai <hardeep.rai@gmail.com>
2024-08-25 20:35:07 +0530

    To date is calculated from loan duration

--- library_management_app/library_management_app/doctype/library_membership/library_membership.py
+++ library_management_app/library_management_app/doctype/library_membership/library_membership.py
@@ -2,7 +2,6 @@
 # For license information, please see license.txt
 
 import frappe
-
 from frappe.model.document import Document
 from frappe.model.docstatus import DocStatus
 
@@ -21,3 +20,7 @@ class LibraryMembership(Document):
         )
         if exists:
             frappe.throw("There is an active membership for this member")
+
+        # get loan period and compute to_date by adding loan_period to from_date
+        loan_period = frappe.db.get_single_value("Library Settings", "loan_period")
+        self.to_date = frappe.utils.add_days(self.from_date, loan_period or 30)
